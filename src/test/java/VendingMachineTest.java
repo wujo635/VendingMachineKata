799dd9123f5c1fcace2e37b24ca48953ad9d65c0
wujo635 <wujo635@gmailc.om>
2019-05-31 14:56:15 -0400

    Initial project setup, fail sanity check

---
+++ src/test/java/VendingMachineTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VendingMachineTest {
+
+    @Test
+    public void shouldStartWithSanityCheck() {
+        assertEquals(0, 1);
+    }
+}
