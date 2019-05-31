d8277eee451ff489f2d6b4a3bd365c000eaef579
wujo635 <wujo635@gmailc.om>
2019-05-31 16:27:03 -0400

    Pass display after insert nickel

--- src/main/java/com/pillar/kata/VendingMachine.java
+++ src/main/java/com/pillar/kata/VendingMachine.java
@@ -1,11 +1,21 @@
 package com.pillar.kata;
 
 public class VendingMachine {
+
+    private int amountInserted;
+
+    VendingMachine() {
+        this.amountInserted = 0;
+    }
+
     public String getDisplay() {
+        if (amountInserted > 0) {
+            return "0.05";
+        }
         return "INSERT COIN";
     }
 
     public void insert(String coin) {
-
+        amountInserted += 5;
     }
 }
