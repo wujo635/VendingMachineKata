deffe3872011d173d64ba65a7a91f8e87c0c2568
wujo635 <wujo635@gmailc.om>
2019-06-01 17:25:04 -0400

    Pass display Thank You when item vended

--- src/main/java/com/pillar/kata/VendingMachine.java
+++ src/main/java/com/pillar/kata/VendingMachine.java
@@ -3,14 +3,19 @@ package com.pillar.kata;
 public class VendingMachine {
 
     private CoinManager coinManager;
+    private boolean itemVended;
 
     VendingMachine() {
         this.coinManager = new CoinManager();
+        this.itemVended = false;
     }
 
     public String getDisplay() {
         if (coinManager.getValue() > 0) {
             return String.format("%.2f", coinManager.getValue() / 100.0);
+        }
+        if (itemVended) {
+            return "THANK YOU";
         }
         return "INSERT COIN";
     }
@@ -24,6 +29,6 @@ public class VendingMachine {
     }
 
     public void select(String item) {
-
+        this.itemVended = true;
     }
 }
