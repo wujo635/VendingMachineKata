49e257e6de70374b8cbe76303d2f08cdecfea520
wujo635 <wujo635@gmailc.om>
2019-05-31 23:12:42 -0400

    Pass value checks for CoinManager

---
+++ src/test/java/com/pillar/kata/CoinManagerTest.java
@@ -0,0 +1,28 @@
+package com.pillar.kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CoinManagerTest {
+
+    private CoinManager coinManager = new CoinManager();
+
+    @Test
+    public void shouldAcceptNickel() {
+        coinManager.insert("NICKEL");
+        assertEquals(5, coinManager.getValue());
+    }
+
+    @Test
+    public void shouldAcceptDime() {
+        coinManager.insert("DIME");
+        assertEquals(10, coinManager.getValue());
+    }
+
+    @Test
+    public void shouldAcceptQuarter() {
+        coinManager.insert("QUARTER");
+        assertEquals(25, coinManager.getValue());
+    }
+}
