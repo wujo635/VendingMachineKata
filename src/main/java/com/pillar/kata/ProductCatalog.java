f9bf689a8b82ee4bf8541368398e947b2ebc281e
wujo635 <wujo635@gmail.com>
2019-06-08 19:02:02 -0400

    Pass allow add item given name and price

--- src/main/java/com/pillar/kata/ProductCatalog.java
+++ src/main/java/com/pillar/kata/ProductCatalog.java
@@ -1,8 +1,20 @@
 package com.pillar.kata;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductCatalog {
 
+    private Map<String, Integer> itemPriceMap;
+
+    ProductCatalog() {
+        itemPriceMap = new HashMap<>();
+    }
+
     public int getPrice(String name) {
+        if (itemPriceMap.containsKey(name)) {
+            return itemPriceMap.get(name);
+        }
         if (name.equals("COLA")) {
             return 100;
         }
@@ -10,6 +22,6 @@ public class ProductCatalog {
     }
 
     public void add(String name, int price) {
-
+        itemPriceMap.put(name, price);
     }
 }
